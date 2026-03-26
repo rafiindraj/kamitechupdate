e28c8f9ff2b6dc26ef430cc2cf71634875fb8f18
rafiindraj <99.rafiindrajati@gmail.com>
2026-03-26 11:56:28 +0700

    material styles experiment

--- src/app/page/page.component.ts
+++ src/app/page/page.component.ts
@@ -14,9 +14,9 @@ import { CommonModule } from '@angular/common';
 export class PageComponent {
   @Input() pageId: string = '';
   @Input() watermarkOpacity: string = '0.02';
-  @Input() contentClass: string = 'flex flex-col text-justify font-serif text-[16px] text-slate-700';
+  @Input() contentClass: string = 'flex flex-col text-justify font-sans text-[16px] text-slate-700';
   @Input() footerText: string = '';
   @Input() footerTextClass: string = 'font-sans text-[#64748B]';
   @Input() pageNum: string = '';
-  @Input() pageNumClass: string = 'font-serif font-bold text-slate-400';
+  @Input() pageNumClass: string = 'font-sans font-bold text-slate-400';
 }
